5c4c7618c96bdfa320fd127a06c0243d4214a57f
nkalkovaliev <nikola.kalkovaliev@endava.com>
2023-02-08 23:30:07 +0100

    Add project files.

---
+++ WiredBrainCoffee.UI/Admin/OrderHistory.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.QuickGrid;
+using WiredBrainCoffee.Models;
+using WiredBrainCoffee.UI.Services;
+
+namespace WiredBrainCoffee.UI.Admin
+{
+    public partial class OrderHistory
+    {
+        [Inject]
+        public IOrderService OrderService { get; set; }
+
+        IQueryable<Order> items = new List<Order>().AsQueryable();
+
+        PaginationState pagination = new PaginationState { ItemsPerPage = 4 };
+
+        string nameFilter = string.Empty;
+
+        IQueryable<Order> FilteredItems => items
+            .Where(x => x.LastName.Contains(nameFilter, StringComparison.CurrentCultureIgnoreCase));
+
+        protected override async Task OnInitializedAsync()
+        {
+            items = (await OrderService.GetOrders()).AsQueryable();
+        }
+    }
+}
---
+++ WiredBrainCoffee.UI/Pages/Index.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WiredBrainCoffee.UI.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        NavigationManager NavManager { get; set; }
+        public void ApplyPromo()
+        {
+            NavManager.NavigateTo("/order", new NavigationOptions()
+            {
+                HistoryEntryState = "WiredDiscount"
+            });
+        }
+    }
+}
